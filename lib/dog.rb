24310860d39ce3877a9ed7e99b0c34c9c86cdd41
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-08 16:31:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,23 @@
-class Dog 
-  
-  def name = (name)
+class Dog
+
+   def name=(name)
     @name = name
-  end 
-  
-  def name
+  end
+
+   def name
     @name
   end
-end 
+
+   def breed=(breed)
+    @breed = breed
+  end
+
+   def breed
+    @breed
+  end
+
+
+ end
 
 
 
